dff6233e9d6c6703e319bcb395d61224db26f1e2
Kastalia <48131567+Kastalia@users.noreply.github.com>
2021-09-04 14:07:35 +0300

    улучшил логгирование. WS работает как надо, мануал отключен

--- src/OrderProto.py
+++ src/OrderProto.py
@@ -5,6 +5,11 @@ import math
 
 import logging
 
+
+WARNING_NOT_CLOSED_ORDER="NOT_CLOSED_ORDER"
+WARNING_UNEXPECTED_WS="UNEXPECTED_WS"
+
+
 class orderProto():
 
     def __init__(self, api_key, api_secret, enableTestnet, symbol):
@@ -29,7 +34,9 @@ class orderProto():
         self._update_balance()
 
         self.orderId = 0
-        self.orderStatus = "EMPTY"
+        self.orderStatus = "BOT_INIT"
+        self.orderSide = "BOT_INIT"
+        self.orderPrice = 0.0
         self.orderQuantity = 0.0
         self.orderFilledQuantity = 0.0
         self.ordersZombie = []
@@ -37,79 +44,65 @@ class orderProto():
         #_log_format = "[%(asctime)s] %(levelname)-8s - %(name) - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
         _log_format = "[%(asctime)s] %(levelname)-8s %(message)s"
         logging.basicConfig(filename="log", format=_log_format, level=logging.INFO, datefmt="%H:%M:%S %d-%m-%Y")
-        logging.info(
-            f"INIT-{self.orderId} {self.orderStatus} price={0.0} {self.orderFilledQuantity}/{self.orderQuantity} "
-            f"{self.baseAsset}={self.baseAsset_balance} {self.quoteAsset}={self.quoteAsset_balance} "
-            f"BNB={self.bnb_balance}")
+        self._logging_info()
+
 
     def sell(self, price, quantity):
         response = self.connector.order_limit_sell(symbol=self.symbol,
-                                                  price=round(price,self.round_price),
-                                                  # newClientOrderId="BUY_"+self.orderSide+"_"+str(self.timeNow),
-                                                  timeInForce='GTC',
-                                                  quantity=round(quantity,self.round_qty))
+                                                  price=round(price, self.round_price),
+                                                  quantity=round(quantity, self.round_qty),
+                                                  # newClientOrderId=f"{time}",
+                                                  newOrderRespType="RESULT" # "FULL"
+                                                  )
         if self.orderStatus in ["NEW", "PARTIALLY_FILLED"]:
             self.ordersZombie.append(self.orderId)
-            logging.warning(
-                f"NOTCLOSED-{self.orderId} {self.orderStatus} price=- {self.orderFilledQuantity}/{self.orderQuantity} "
-                f"{self.baseAsset}={self.baseAsset_balance} {self.quoteAsset}={self.quoteAsset_balance} "
-                f"BNB={self.bnb_balance}")
-
-        self._update_balance()
+            self._logging_warning(comment=WARNING_NOT_CLOSED_ORDER)
 
         self.orderId = response["orderId"]
-        side = response["side"]
         self.orderStatus = response["status"]
+        self.orderSide = response["side"]
+        self.orderPrice = float(response["price"])
         self.orderQuantity = float(response["origQty"])
         self.orderFilledQuantity = float(response["executedQty"])
-        logging.info(
-            f"{side}-{self.orderId} {self.orderStatus} price={price} {self.orderFilledQuantity}/{self.orderQuantity} "
-            f"{self.baseAsset}={self.baseAsset_balance} {self.quoteAsset}={self.quoteAsset_balance} "
-            f"BNB={self.bnb_balance}")
 
     def sell_stopLoss(self, price, quantity):
         raise NotImplemented
 
     def buy(self, price, quantity):
         response = self.connector.order_limit_buy(symbol=self.symbol,
-                                           price=round(price,self.round_price),
-                                           # newClientOrderId="BUY_"+self.orderSide+"_"+str(self.timeNow),
-                                           timeInForce='GTC',
-                                           quantity=round(quantity,self.round_qty))
+                                                  price=round(price, self.round_price),
+                                                  quantity=round(quantity, self.round_qty),
+                                                  # newClientOrderId=f"{time}",
+                                                  newOrderRespType="RESULT" #"FULL"
+                                                  )
+
         if self.orderStatus in ["NEW", "PARTIALLY_FILLED"]:
             self.ordersZombie.append(self.orderId)
-            logging.warning(
-                f"NOTCLOSED-{self.orderId} {self.orderStatus} price=- {self.orderFilledQuantity}/{self.orderQuantity} "
-                f"{self.baseAsset}={self.baseAsset_balance} {self.quoteAsset}={self.quoteAsset_balance} "
-                f"BNB={self.bnb_balance}")
-
-        self._update_balance()
+            self._logging_warning(comment=WARNING_NOT_CLOSED_ORDER)
 
         self.orderId = response["orderId"]
-        side = response["side"]
         self.orderStatus = response["status"]
-        self.orderQuantity = quantity
+        self.orderSide = response["side"]
+        self.orderPrice = float(response["price"])
+        self.orderQuantity = float(response["origQty"])
         self.orderFilledQuantity = float(response["executedQty"])
-        logging.info(
-            f"{side}-{self.orderId} {self.orderStatus} price={price} {self.orderFilledQuantity}/{self.orderQuantity} "
-            f"{self.baseAsset}={self.baseAsset_balance} {self.quoteAsset}={self.quoteAsset_balance} "
-            f"BNB={self.bnb_balance}")
 
     def buy_stopLoss(self, price, quantity):
         raise NotImplemented
 
     def cancel(self):
         response = self.connector.cancel_order(symbol=self.symbol, orderId=self.orderId)
-        self._update_balance()
-
-        side = response["side"]
-        price = response["price"]
         self.orderStatus = response["status"]
         self.orderFilledQuantity = float(response["executedQty"])
-        logging.info(
-            f"{side}-{self.orderId} {self.orderStatus} price={price} {self.orderFilledQuantity}/{self.orderQuantity} "
-            f"{self.baseAsset}={self.baseAsset_balance} {self.quoteAsset}={self.quoteAsset_balance} "
-            f"BNB={self.bnb_balance}")
+
+    def cancel_zombies(self):
+        for orderId in self.ordersZombie:
+            response_status = self.connector.get_order(symbol=self.symbol, orderId=orderId, recvWindow=60000)
+            if response_status["status"] in ["NEW", "PARTIALLY_FILLED"]:
+                response = self.connector.cancel_order(symbol=self.symbol, orderId=orderId)
+                self._logging_info_response(response=response)
+        self.ordersZombie = []
+
 
     def transposition_sellBuy(self):
         tmp = self.buy
@@ -139,24 +132,17 @@ class orderProto():
 
     def _callback_userdatastreams(self, msg):
         eventType = msg["e"]
-        if eventType == "executionReport":
-            # проблема кучи ловушек и кучи монет
-            # проверка соответствия id
-            if (msg["i"] != self.orderId):
-                return
-            # Сообщение об исполнении https://binance-docs.github.io/apidocs/spot/en/#payload-order-update
-            orderId = msg["i"]
-            #transactionTime = msg["T"]
-            # могут быть статусы:
-            # NEW\TRADE(FILLED или PARTIALLY_FILLED)\CANCELED\REPLACED(не использ.)\REJECTED\EXPIRED
-            #executionType = msg["x"]
-            self.orderStatus = msg["X"]
-            # сколько заполнено
-            self.orderFilledQuantity = float(msg["z"])
-            #смен полей класса!
+        if (eventType == "executionReport"):
+            if (msg["i"] == self.orderId):
+                #transactionTime = msg["T"]
+                #executionType = msg["x"]
+                self.orderStatus = msg["X"]
+                self.orderFilledQuantity = float(msg["z"])
+                self._logging_info()
 
         elif eventType == "outboundAccountPosition":
             # Информация о балансе аккаунта: все монеты и free\locked
+            # Возможно он будет просираться на каждую монету и инстанс и засрет весь лог балансом
             for asset_info in msg["B"]:
                 if asset_info["a"] == self.baseAsset:
                     self.baseAsset_balance = float(asset_info["f"])
@@ -164,7 +150,7 @@ class orderProto():
                     self.quoteAsset_balance = float(asset_info["f"])
                 elif asset_info["a"] == "BNB":
                     self.bnb_balance = float(asset_info["f"])
-
+            self._logging_info_balance()
 
         elif eventType == "balanceUpdate":
             # обновление баланса
@@ -175,31 +161,72 @@ class orderProto():
             return
 
         else:
+            self._logging_warning(comment=WARNING_UNEXPECTED_WS)
             # такого быть не может
             return
 
     def _pollStatus(self, timeout=0.1):
-        while(self.pollStatus_continue):
-            if self.orderStatus in ["EMPTY", "FILLED", "CANCELED","PENDING_CANCEL","REJECTED","EXPIRED"]:
-                sleep(timeout)
-                continue
-            response = self.connector.get_order(symbol=self.symbol, orderId=self.orderId, recvWindow=60000)
-            executedQty = float(response["executedQty"])
-            if self.orderFilledQuantity != executedQty:
-                self._update_balance()
-
-                side = response["side"]
-                price = response["price"]
-                self.orderStatus = response["status"]
-                self.orderFilledQuantity = executedQty
-                logging.info(
-                    f"{side}-{self.orderId} {self.orderStatus} price={price} {self.orderFilledQuantity}/{self.orderQuantity} "
-                    f"{self.baseAsset}={self.baseAsset_balance} {self.quoteAsset}={self.quoteAsset_balance} "
-                    f"BNB={self.bnb_balance}")
-            sleep(timeout)
+        raise NotImplemented
+        # orderId_prev = self.orderId
+        # orderStatus_prev = self.orderStatus
+        # while(self.pollStatus_continue):
+        #     if self.orderStatus == "BOT_INIT":
+        #         sleep(timeout)
+        #         continue
+        #     # if self.orderStatus in ["BOT_INIT", "FILLED", "CANCELED","PENDING_CANCEL","REJECTED","EXPIRED"]:
+        #     #     orderId_prev = self.orderId
+        #     #     sleep(timeout)
+        #     #     continue
+        #     response = self.connector.get_order(symbol=self.symbol, orderId=self.orderId, recvWindow=60000)
+        #     executedQty = float(response["executedQty"])
+        #     if (self.orderId != orderId_prev) or\
+        #             (self.orderStatus != orderStatus_prev) or\
+        #             (self.orderFilledQuantity != executedQty):
+        #         self._update_balance()
+        #         self.orderStatus = response["status"]
+        #         self.orderFilledQuantity = executedQty
+        #         self._logging_info()
+        #     orderId_prev = self.orderId
+        #     orderStatus_prev = self.orderStatus
+        #     sleep(timeout)
 
     def _update_balance(self):
         self.baseAsset_balance = float(self.connector.get_asset_balance(self.baseAsset)['free'])
         self.quoteAsset_balance = float(self.connector.get_asset_balance(self.quoteAsset)['free'])
         self.bnb_balance = float(self.connector.get_asset_balance("BNB")['free'])
+
+    def _logging_info(self):
+        logging.info(
+            f"{self.orderSide}-{self.orderId} {self.orderStatus} price={self.orderPrice} "
+            f"{self.orderFilledQuantity}/{self.orderQuantity} "
+            f"{self.baseAsset}={self.baseAsset_balance} {self.quoteAsset}={self.quoteAsset_balance} "
+            f"BNB={self.bnb_balance}")
+
+    def _logging_info_response(self, response):
+        orderId = response["orderId"]
+        orderStatus = response["status"]
+        orderSide = response["side"]
+        orderPrice = float(response["price"])
+        orderQuantity = float(response["origQty"])
+        orderFilledQuantity = float(response["executedQty"])
+        logging.info(
+            f"{orderSide}-{orderId} {orderStatus} price={orderPrice} "
+            f"{orderFilledQuantity}/{orderQuantity} "
+            f"{self.baseAsset}={self.baseAsset_balance} {self.quoteAsset}={self.quoteAsset_balance} "
+            f"BNB={self.bnb_balance}")
+
+    def _logging_info_balance(self):
+        logging.info(
+            f"UPDATE BALANCE: "
+            f"{self.baseAsset}={self.baseAsset_balance} {self.quoteAsset}={self.quoteAsset_balance} "
+            f"BNB={self.bnb_balance}")
+
+    def _logging_warning(self, comment):
+        logging.warning(
+            f"{comment} {self.orderSide}-{self.orderId} {self.orderStatus} price={self.orderPrice} "
+            f"{self.orderFilledQuantity}/{self.orderQuantity} "
+            f"{self.baseAsset}={self.baseAsset_balance} {self.quoteAsset}={self.quoteAsset_balance} "
+            f"BNB={self.bnb_balance}")
+
+
 
