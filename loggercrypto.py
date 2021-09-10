a2db38bb47ccc8795c5fd1dbebbe52f2cb2da74e
Kastalia <48131567+Kastalia@users.noreply.github.com>
2021-09-11 00:45:35 +0300

    исправил баг с дубликатами логов (propagate=False)

--- loggercrypto.py
+++ loggercrypto.py
@@ -10,12 +10,14 @@ LOG_FILE_BACKUP_COUNT = 5
 
 def _get_console_handler():
     console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.INFO)
     console_handler.setFormatter(FORMATTER)
     return console_handler
 
 
 def _get_file_handler():
     file_handler = RotatingFileHandler(LOG_FILE, maxBytes=LOG_FILE_SIZE, backupCount=LOG_FILE_BACKUP_COUNT)
+    file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(FORMATTER)
     return file_handler
 
@@ -25,5 +27,6 @@ def get_logger(logger_name):
     logger.setLevel(logging.INFO)
     logger.addHandler(_get_console_handler())
     logger.addHandler(_get_file_handler())
+    logger.propagate = False
     return logger
 
